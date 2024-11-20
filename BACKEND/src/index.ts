0a3cf20246703848db8060695228ac6acbec6e15
Ashita <ashitam2407@gmail.com>
2024-11-20 09:11:47 +0530

    structure of routes

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -1,3 +1,19 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("./api/vi/signup", (req, res) => {});
+
+app.post("/api/v1/signin", (req, res) => {});
+
+app.post("/api/v1/content", (req, res) => {});
+
+app.get("/api/v1/content", (req, res) => {});
+
+app.post("/api/v1/link/share", (req, res) => {});
+
+app.get("/api/v1/link/:sharelink", (req, res) => {});
