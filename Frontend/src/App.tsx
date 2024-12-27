8e852ee32131cba22119efce20f8d4f4fc82ddc1
Ashita <ashitam2407@gmail.com>
2024-12-27 06:21:11 +0530

    added routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,18 @@
-// import { Dashboard } from "./pages/Dashboard";
+import { Dashboard } from "./pages/Dashboard";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App: React.FC = () => {
-  return <Signin />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
